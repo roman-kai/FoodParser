92e5e3bd1853bfe93c68dc87f2ff546576755ad7
Kolomenskii <kolomens@cs.karelia.ru>
2015-08-25 02:24:01 +0300

    Added food parser project for english language
    FoodParser based on dish/unit lists and OpenNLP POSTagger

---
+++ src/Fact.java
@@ -0,0 +1,11 @@
+
+public class Fact {
+	public int factID = -1;
+	public int countID = -1;
+	public int start;
+	public int length;
+	
+	public Fact(int countId){
+		this.countID = countId;
+	}
+}
---
+++ src/Main.java
@@ -0,0 +1,36 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+
+public class Main {
+
+	public static boolean isTest = false;
+	public static boolean isDebug = true;
+	
+	public static void main(String[] args) {
+		if(args.length > 0 && args[0].equalsIgnoreCase("test")){
+			isTest = true;
+			Log.t("Running in test mode...");
+		}
+		FoodParser fp;
+		fp = new FoodParser();
+		DataLoader dl = new DataLoader(fp);
+		dl.loadConfig();
+		dl.loadResources();
+		BufferedReader br;
+		br = new BufferedReader(new InputStreamReader(System.in));
+		
+			try {
+				String s = br.readLine();
+				if(s != null)
+					fp.processPhrase(s);
+				
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+	}
+
+}
